3cf9eb558dca0b9936cdf9d834cf858629b7d1f2
Sarah Zhao <sarahzhao25@gmail.com>
2020-02-03 11:45:00 -0500

    added template

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<div>Hi</div>, document.getElementById('app'));
+// route path to some component
+
+const HarryLand = () => {
+  return <div>Expelliarmus!</div>
+}
+
+const HamiltonLand = (props) => {
+  console.log('these are my hamiltonprops', props);
+  return <div>I am not throwing away my shot, {props.enemy}!</div>
+}
+
+const Home = () => {
+  return <div>I am on the home page!</div>
+}
+
+const RenderHamilton = (routeProps) => {
+  return <HamiltonLand {...routeProps} />
+}
+
+const Main = () => {
+  return (
+    <HashRouter>
+      <div>
+        <Link to="/harry">Click for Harry!</Link>
+        <Link to="/hamilton">Click for Hamilton!</Link>
+        <Route path="/harry" component={HarryLand} />
+        <Route path="/hamilton" render={(routeProps) => <HamiltonLand {...routeProps} enemy={'Aaron Burr'}/>} />
+        <Route exact path="/" component={Home} />
+      </div>
+    </HashRouter>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('app'));
